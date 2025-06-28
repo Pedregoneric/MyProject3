b592df689e1931d90b8cdb0343b8098221381c3a
Pedregoneric <117611099+Pedregoneric@users.noreply.github.com>
2025-06-28 10:49:40 -0700

    initial commit
    initial commit

---
+++ Source/MyProject3/MyProject3.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MyProject3 : ModuleRules
+{
+	public MyProject3(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"MyProject3",
+			"MyProject3/Variant_Platforming",
+			"MyProject3/Variant_Combat",
+			"MyProject3/Variant_Combat/AI",
+			"MyProject3/Variant_SideScrolling",
+			"MyProject3/Variant_SideScrolling/Gameplay",
+			"MyProject3/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/MyProject3/MyProject3GameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyProject3GameMode.h"
+
+AMyProject3GameMode::AMyProject3GameMode()
+{
+	// stub
+}
